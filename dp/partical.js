6b3f5cb13ab3bda1224565b0bd9a38512161002a
xyz <1351998071@qq.com>
2018-09-29 08:16:22 +0800

    23123123

--- dp/partical.js
+++ dp/partical.js
@@ -1,16 +1,19 @@
-function createPartical(x, y) {
+function CreatePartical(x, y) {
    var p = {
       x: y,
       y: y,
    }
    //要什么new 弃了
-   p.constructor = createPartical
-   p.__proto__ = createPartical.prototype
+   p.constructor = CreatePartical
+   p.__proto__ = CreatePartical.prototype
    p.__proto__.init = function(vx, vy, size) {
       this.vx = vx
       this.vy = vy
       this.w = size
       this.h = size
+   }
+   p.__proto__.draw = function(canvas, context) {
+
    }
    return p;
 }
--- dp/utils.js
+++ dp/utils.js
@@ -16,3 +16,147 @@ function webInit() {
       return this.previousElementSibling
    }
 }
+
+/**
+ * 为指定的canvas 生成动画
+ */
+function Animation(canvas) {
+   var a = {
+      cvs: canvas,
+      ctx: canvas.getContext("2d"),
+      fps: 60, //fps 只能是整十数
+      time: 0,
+      element: [],
+   }
+   a.constructor = Animation
+   a.__proto__ = Animation.prototype
+   Animation.prototype.timer = function() {
+      //取余对小数也成立 惊了 不会有bug 吧
+      if(this.time++ % (60 / this.fps) == 0) {
+         //每秒按照一定间隔 在canvas上画出 自个element里带有draw()方法的对象 并且更新他们的数据
+         for(var i = this.element.length - 1; i >= 0; i--) {
+            var item = this.element[i]
+            var index = i
+            if(item.exist) {
+               item.updata()
+               item.draw(this.canvas, this.ctx)
+            }else {
+               this.element.splice(index, 1)
+            }
+         }
+      }
+      window.requestAnimationFrame(() => {
+         this.timer()
+      })
+   }
+   a.timer()
+   return a
+}
+
+/**
+ * 判定所指定的矩形是否在window里
+ */
+function isOutsideOfWindow(x, y, w, h) {
+    return !( x >= 0
+       && (x + w) <= window.innerWidth
+       && y >= 0
+       && (y + h) <= window.innerHeight
+    )
+}
+
+/**
+ * 使一个dom充满window
+ */
+function fullWindow(dom) {
+   dom.width = dom.width ? dom.width : dom.offsetWidth
+   dom.height = dom.height ? dom.height : dom.offsetHeight
+   dom.width = window.innerWidth + "px"
+   dom.height = window.innerHeight + "px"
+}
+
+/**
+ * element
+ */
+function setElement(place, element) {
+   place.append(element)
+   return element
+}
+function getParentWidth(element) {
+   return element.parentElement.offsetWidth
+}
+function getParentHeight(element) {
+   return element.parentElement.offsetHeight
+}
+function setElementHeight(element, height) {
+   element.style.height = height + "px"
+   element.height = height
+   return element
+}
+function setElementWidth(element, width) {
+   element.style.width = width + "px"
+   element.width = width
+   return element
+}
+function elementFull(element) {
+   setElementWidth(element, getParentWidth(element))
+   setElementHeight(element, getParentHeight(element))
+   return element
+}
+
+/**
+ * canvs 用类? 重构?
+ */
+// function createCanvas() {
+//    return document.createElement("canvas")
+// }
+// function getContext(canvas) {
+//    return canvas.getContext("2d")
+// }
+// function fill(canvas, style) {
+//    var ctx = getContext(canvas)
+//    ctx.fillStyle = style
+//    ctx.fill()
+// }
+// function stroke(canvas, style) {
+//    var ctx = getContext(canvas)
+//    ctx.strokeStyle = style
+//    ctx.stroke()
+// }
+// function drawPoint(canvas, x, y, r = 2, color = "black") {
+//    var ctx = getContext(canvas)
+//    ctx.beginPath()
+//    ctx.arc(x, y, r, 0, Math.PI * 2)
+//    ctx.fillStyle = color
+//    ctx.fill()
+//    return canvas
+// }
+// function drawPoints(canvas, points) {
+//    points.forEach(function(item) {
+//       drawPoint(canvas, item.x, item.y, item.r, item.color)
+//    })
+//    return canvas
+// }
+// function curveOfPath(canvas, dots = []) {
+//    if(dots.length != 3) {
+//       log("the length of dots aren't three in " + (curveOfPath.caller ? curveOfPath.caller : curveOfPath.name))
+//    }else {
+//       var ctx = getContext(canvas)
+//       ctx.bezierCurveTo(dots[0].x, dots[0].y, dots[1].x, dots[1].y, dots[2].x, dots[2].y)
+//    }
+//    return canvas
+// }
+// function ctxTrans(canvas, x, y) {
+//    getContext(canvas).translate(x,y)
+// }
+// function ctxRotate(canvas, angle) {
+//    getContext(canvas).rotate(angle)
+// }
+// function ctxSave(canvas) {
+//    getContext(canvas).save()
+// }
+// function ctxRestore(canvas) {
+//    getContext(canvas).restore()
+// }
+// function ctxClosePath(canvas) {
+//    getContext(canvas).closePath()
+// }
