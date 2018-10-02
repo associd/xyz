f978a85cc329bd2b0371971821369135f80b52db
xyz <1351998071@qq.com>
2018-10-02 11:56:53 +0800

    10-2

--- dp/partical.js
+++ dp/partical.js
@@ -5,31 +5,45 @@ function CreatePartical(x, y) {
       color: "black",
       vx: 0,
       vy: 0,
+      ax: 0,
+      ay: 0,
       w: 1,
       h: 1,
+      time: 0,
       exist: true,
    }
-   //要什么new 弃了
+
    p.constructor = CreatePartical
    p.__proto__ = CreatePartical.prototype
-   p.__proto__.init = function(vx, vy, size) {
-      this.vx = vx
-      this.vy = vy
-      this.w = size
-      this.h = size
-   }
-   p.__proto__.draw = function(canvas, context) {
-      context.beginPath()
-      context.fillStyle = this.color
-      context.fillRect(this.x, this.y, this.w, this.h)
-   }
-   p.__proto__.update = function(canvas, context) {
-      if(this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
-         this.exist = false
-         // log("in")
+
+   if(p.__proto__.__DEFINE__ == undefined) {
+      p.__proto__.__DEFINE__ = true
+      p.__proto__.init = function(vx, vy, ax, ay, size) {
+         this.vx = vx
+         this.vy = vy
+         this.ax = ax
+         this.ay = ay
+         this.w = size
+         this.h = size
       }
-      this.x += this.vx
-      this.y += this.vy
+      p.__proto__.draw = function(canvas, context) {
+         context.beginPath()
+         context.fillStyle = this.color
+         context.fillRect(this.x, this.y, this.w, this.h)
+      }
+      p.__proto__.update = function(canvas, context) {
+         // if(this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
+         //    this.exist = false
+         //    // log("in")
+         // }
+         if(this.time++ >= 60 * 2) {
+            this.exist = false
+         }
+         this.vx += this.ax
+         this.vy += this.ay
+         this.x += this.vx
+         this.y += this.vy
+      }
    }
 
    return p;
