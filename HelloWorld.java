5aefd8465123ef17be59f42cdb07b8766b411511
xyz <1351998071@qq.com>
2018-12-06 19:51:59 +0800

    12231

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//注释
+public class HelloWorld {
+   public static void main(String[] args) {
+      int num = 100;
+      System.out.println("hello World!!!!!!");
+      System.out.println(100);
+   }
+}
