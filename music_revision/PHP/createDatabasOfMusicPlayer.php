7145d0cab3a2ff5da14eb80fa9d6c72893a170e5
xyz <1351998071@qq.com>
2019-03-27 09:44:43 +0800

    i find that goushi

--- music_revision/PHP/createDatabasOfMusicPlayer.php
+++ music_revision/PHP/createDatabasOfMusicPlayer.php
@@ -2,4 +2,15 @@
 
 $DB = new Mysqli("localhost", "root", "");
 
-$DB->query("CREATE DATABASE MusicPlayer");
+$res = $DB->multi_query("
+   CREATE DATABASE IF NOT EXISTS MusicPlayer CHARACTER SET UTF8;
+   USE MusicPlayer;
+   CREATE TABLE IF NOT EXISTS Music (
+      id int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      name varchar(200),
+      image varchar(200),
+      author varchar(200)
+   );
+");
+
+echo $DB->error;
