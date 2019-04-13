e1ce4568ccc0aa67e039e1f7596532ae7244200a
xyz <1351998071@qq.com>
2019-04-13 19:39:22 +0800

    clear up

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-//注释
-public class HelloWorld {
-   public static void main(String[] args) {
-      int num = 100;
-      System.out.println("hello World!!!!!!");
-      System.out.println(100);
-   }
-}
