7e8031c12124e4c52540842aa76d62ffa0a04189
xyz <1351998071@qq.com>
2019-05-04 18:48:53 +0800

    sadfasdf

--- JS/Module/main.js
+++ JS/Module/main.js
@@ -32,19 +32,19 @@ function main () {
       for( var i = 0; i < numberOfcardPerWeft[j]; i++ ) {
          var div = document.createElement("div")
          div.className = "card"
-         div.style.transform = `rotateY(${angleX * i}deg) rotateX(${(j - 5) * angleY}deg) translateZ(1000px)`;
+         div.style.transform = `rotateY(${angleX * i}deg) rotateX(${(j - 5) * angleY}deg) translateZ(700px)`;
          container.append(div)
          cards.push({
             element: div,
             transform: {
                rotateY: angleX * i,
                rotateX: (j - 5) * angleY,
-               translateZ: "1000px",
+               translateZ: "700px",
             },
             direction: direction,
             update: function() {
                this.transform.rotateY += this.direction
-               this.element.style.transform = `rotateY(${this.transform.rotateY}deg) rotateX(${this.transform.rotateX}deg) translateZ(800px)`;
+               this.element.style.transform = `rotateY(${this.transform.rotateY}deg) rotateX(${this.transform.rotateX}deg) translateZ(700px)`;
             },
          })
       }
