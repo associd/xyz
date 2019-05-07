b39de76059b29223b44f22989bbc6390f35172b8
xyz <1351998071@qq.com>
2019-05-07 14:20:21 +0800

    2312132

--- HTML/Assignment/JS/master.js
+++ HTML/Assignment/JS/master.js
@@ -1,15 +1,36 @@
 window.onload = function() {
    var gradient = 50
    es(".card").forEach( (item, index, array) => {
-      item.dataset.order = index;
+      item.order = index;
       item.style.backgroundColor = `rgb(${gradient * (index + 1)}, ${gradient * (index + 1)}, ${gradient * (index + 1)})`;
       item.style.color = `rgb(${gradient * (index + 1)}, ${gradient * (index + 1)}, ${gradient * (index + 1)})`;
-      item.style.animation = `growth 1.5s ${index * 0.1 + 0.5}s cubic-bezier(.77,0,.18,1) forwards`;
-      item.transmitHover = function(nextNodeOrder, energy) {
-         if(this.dataset.order == 0 || this.dataset.order == array.length) {
-            this.style.height = this.offsetHeight * energy
-            this.style.width = this.offsetWidth * energy
-         }else if(this.dataset.order < )
+      setTimeout( () => {
+         item.style.height = "100%";
+      }, index * 100 + 500)
+      item.resum = function() {
+         this.style.height = "100%";
+      }
+      item.transmitRight = function(energy) {
+         this.style.height = energy * 100 + "%"
+         this.style.width = energy * 100 + "%"
+         if( this.order + 1 < array.length ) {
+            array[this.order + 1]["transmitRight"]( energy * 0.9 )
+         }
+      }
+      item.transmitLeft = function(energy) {
+         this.style.height = energy * 100 + "%"
+         this.style.width = energy * 100 + "%"
+         if( this.order - 1 >= 0 ) {
+            array[this.order - 1]["transmitLeft"]( energy * 0.9 )
+         }
+      }
+      item.transmitHover = function(deriction, energy) {
+         if(deriction == "right") {
+            this.transmitRight(energy)
+         }
+         if(deriction == "left") {
+            this.transmitLeft(energy)
+         }
       }
    } )
    e(".container").addEventListener("click", event => {
@@ -18,11 +39,20 @@ window.onload = function() {
       e(`#${event.target.dataset.assgin}`).classList.add("show")
    })
    e(".container").addEventListener("mouseleave", event => {
-      log("sadfsadfsadfsadf")
+      es(".card").forEach(function(item) {
+         item.resum()
+      })
    })
    e(".container").addEventListener("mouseover", event => {
-      if(event.target.classList.contains("card")) {
-
+      let target = event.target
+      let index = target.order
+      if(target.classList.contains("card")) {
+         if( index < es(".card").length ) {
+            target.transmitHover("right", 1.2)
+         }
+         if( index > 0 ) {
+            target.transmitHover("left", 1.2)
+         }
       }
    })
 }
