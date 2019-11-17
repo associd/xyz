11b6a7f05cdf4f7ec2f153c9150bc762428c3fd6
xyz <1351998071@qq.com>
2019-11-17 13:46:45 +0800

    del

--- C/checkString.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-   char * a[10000];
-   char * b[10000];
-   scanf("%s\n%s", a, b);
-   printf("%s\n%s", a, b);
-   return 0;
-}
--- C/printN.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-void PrintN(int N) {
-  while(N > 0) {
-    printf("%d",N--);
-  }
-}
